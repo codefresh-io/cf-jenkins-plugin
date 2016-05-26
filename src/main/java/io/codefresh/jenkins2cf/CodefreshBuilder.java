02a306641a2679086ef535ae2cca5f0acf1612e5
Ant Weiss <ant.weiss@gmail.com>
2016-05-26 16:05:49 +0300

    non-working with restlet

---
+++ src/main/java/io/codefresh/jenkins2cf/CodefreshBuilder.java
@@ -0,0 +1,174 @@
+package io.codefresh.jenkins2cf;
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.util.FormValidation;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.model.AbstractProject;
+import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.util.Secret;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.QueryParameter;
+import org.restlet.Client;
+import org.restlet.data.Protocol;
+import java.io.IOException;
+import java.util.Map;
+import org.restlet.data.Form;
+import org.restlet.data.Method;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.util.Engine;
+//import org.restlet.ext.json.JsonRepresentation;
+
+/**
+ * Sample {@link Builder}.
+ *
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link CodefreshBuilder} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields (like {@link #name})
+ * to remember the configuration.
+ *
+ * <p>
+ * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)}
+ * method will be invoked.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class CodefreshBuilder extends Builder {
+
+    private final boolean launch;
+
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    @DataBoundConstructor
+    public CodefreshBuilder(Boolean launch) {
+        this.launch = launch;
+
+    }
+
+    /**
+     * We'll use this from the <tt>config.jelly</tt>.
+     */
+    public boolean getLaunch() {
+        return launch;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+
+           Client client = new Client(Protocol.HTTP);
+
+            Request request = new Request (Method.GET, "https://g.codefresh.io/api/builds/571c8058b3a58f06001d28a1" );
+
+            final String RESTLET_HTTP_HEADERS = "org.restlet.http.headers";
+
+            Map<String, Object> reqAttribs = request.getAttributes();
+            Form headers = (Form)reqAttribs.get(RESTLET_HTTP_HEADERS);
+            if (headers == null) {
+                headers = new Form();
+                reqAttribs.put(RESTLET_HTTP_HEADERS, headers);
+            }
+            headers.add("x-access-token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NmVlOGYwY2FiNzkwYjA2MDAyODEzYzciLCJhY2NvdW50SWQiOiI1NmVlOGYwY2FiNzkwYjA2MDAyODEzYzgiLCJpYXQiOjE0NjExNTUwNjAsImV4cCI6MTQ2Mzc0NzA2MH0.LYDHyMK8eanWFczgV15GaXIpqenA3_GzeWE76ZNtBKE");
+
+            request.getClientInfo().setAgent("myAgent");
+
+            Response r = client.handle(request);
+
+            listener.getLogger().println("\nJSON representation");
+
+            listener.getLogger().println(r.getEntity());
+
+        return true;
+    }
+
+    // Overridden for better type safety.
+    // If your plugin doesn't really define any property on Descriptor,
+    // you don't have to do this.
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+    }
+
+    /**
+     * Descriptor for {@link CodefreshBuilder}. Used as a singleton.
+     * The class is marked as public so that it can be accessed from views.
+     *
+     * <p>
+     * See <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+     * for the actual HTML fragment for the configuration screen.
+     */
+    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        /**
+         * To persist global configuration information,
+         * simply store it in a field and call save().
+         *
+         * <p>
+         * If you don't want fields to be persisted, use <tt>transient</tt>.
+         */
+        private String cfUser;
+        private Secret cfToken;
+
+        /**
+         * Performs on-the-fly validation of the form field 'name'.
+         *
+         * @param value
+         *      This parameter receives the value that the user has typed.
+         * @return
+         *      Indicates the outcome of the validation. This is sent to the browser.
+         */
+//        public FormValidation doCheckUser(@QueryParameter String value)
+//                throws IOException, ServletException {
+//            if (value.length() == 0)
+//                return FormValidation.error("Please set a name");
+//            if (value.length() < 4)
+//                return FormValidation.warning("Isn't the name too short?");
+//            return FormValidation.ok();
+//        }
+
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            // Indicates that this builder can be used with all kinds of project types
+            return true;
+        }
+
+        /**
+         * This human readable name is used in the configuration screen.
+         */
+        public String getDisplayName() {
+            return "Define Codefresh Integration";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+            // To persist global configuration information,
+            // set that to properties and call save().
+            cfUser = formData.getString("cfUser");
+            cfToken = Secret.fromString(formData.getString("cfToken"));
+
+            // ^Can also use req.bindJSON(this, formData);
+            //  (easier when there are many fields; need set* methods for this, like setUseFrench)
+            save();
+            return super.configure(req,formData);
+        }
+
+        /**
+         * This method returns true if the global configuration says we should speak French.
+         *
+         * The method name is bit awkward because global.jelly calls this method to determine
+         * the initial state of the checkbox by the naming convention.
+         * @return
+         */
+        public String getCfUser() {
+            return cfUser;
+        }
+
+        public Secret getCfToken() {
+            return cfToken;
+        }
+    }
+}
